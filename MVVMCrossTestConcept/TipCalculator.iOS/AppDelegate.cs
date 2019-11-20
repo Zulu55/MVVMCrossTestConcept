d1f5e0a0229d1bda029919b34255f87d6d8939ab
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2019-11-20 09:23:34 -0500

    Ready Android pending to test in iOS

---
+++ MVVMCrossTestConcept/TipCalculator.iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TipCalculator.Core;
+
+namespace TipCalculator.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+    }
+}
